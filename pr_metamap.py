96231904857be5590b70880277a6dc81460d44d3
Chun Feng <weilin0933602899@gmail.com>
2020-09-22 22:11:47 +0800

    加入功能 : 去除重複, 加註解

--- pr_metamap.py
+++ pr_metamap.py
@@ -6,10 +6,11 @@ sents2 = ['John had a huge heart attack and fever']
 # sents = ['cold', "John had huge heart attack and fever"]
 txt_file = "clinical_txt/1.txt"
 
-
+# read a file line by line to a List
 def read_line(txt_file):
     sentences = list()
     with open(txt_file, 'r') as f:
+        # i = index number, l = 讀出的某一行內容
         for i, l in enumerate(f):
             sentences.append(l)
         lines = i + 1
@@ -18,24 +19,31 @@ def read_line(txt_file):
 
 sents, lines = read_line(txt_file)
 index_list = range(1, lines+1)
+# 將剛剛讀出的行內容List, 傳給 mm.extract_concepts 取出概念
 concepts, error = mm.extract_concepts(
     sents, index_list, word_sense_disambiguation=True)
 
 str = []
-n = 0
+number_for_everyone = 0
+# 每個 concept 都有的數值, 紀錄現在印到了哪個 index
+# 每個 index 只會印一次
+whether_print = True
+# 如果印出了 index 就不要印了 設為否
 for concept in concepts:
-    # if n != int(concept.index):
     '''
     Concept : 
     Index 儲存行的資訊
     semtype 儲存 semantic type 的資訊
-    '''
-    if n != int(concept.index):
-        print("Index:"+concept.index)
-        n += 1
+    '''    
+    if number_for_everyone != int(concept.index) :
+        whether_print = True
+        number_for_everyone += 1 
     if concept.semtypes == "[clnd]" or concept.semtypes == "[dsyn]" :
+        if whether_print:
+            print("Index:"+concept.index)
+            whether_print =False
+        
         print("CUI:"+concept.cui, "SMT:"+concept.semtypes, "POS:"+concept.pos_info, 
         "PREFER_N:"+concept.preferred_name, "NEGATED:"+mmip.trigger_parser(concept.trigger)[5], "TRIGGER:"+concept.trigger)
-    
-    
+  
 # Output format is : index, mm, score, preferred_name, cui, semtype, trigger, location, pos_info, tree_nodes
