5087e4a881e740ae7be46ce5d4236af2f6dd6f24
Chun Feng <weilin0933602899@gmail.com>
2020-10-22 11:16:36 +0800

    加入句子處理功能到 mytool

--- mytool/sentenizer.py
+++ mytool/sentenizer.py
@@ -7,3 +7,55 @@
 4. 加入前置 ID
 
 '''
+
+import re
+sen_before = list()
+sen_after = list()
+
+# file = 'clinical_txt/1.txt'
+# out_file = 'ex1_o.txt'
+
+# sen1 = "cycle 2 on 2017.07.14 to 2017.07.1 cycle 3 on 2017.08.11 to 2017.08.13 ;"
+
+def file_process(file_in, file_out):
+    f = open(file_in)
+    lines = f.readlines()
+    print("Input file : ", file_in)
+    # print(type(lines))
+    for line in lines:
+        sen_before.append(line)
+    f.close()
+    # sen_after= [re.sub(pattern, '/', line) for line in sen_before]
+    sen_after = [re.sub(r'(\d{4}) . (\d{2}) . (\d{2}) .', r'\1/\2/\3', line) for line in sen_before]
+
+    sen_after = [re.sub(r'(\d{4}).(\d{2}).(\d{2})', r'\1/\2/\3', line) for line in sen_after]
+
+    sen_after = [re.sub(r'(\d{4}).(\d{2}).(\d{1})', r'\1/\2/\3', line) for line in sen_after]
+
+    sen_after = [re.sub(r'(\d{4}) . (\d{2})', r'\1/\2', line) for line in sen_after]
+    
+    sen_after = [re.sub(r'(\d{4}).(\d{2})', r'\1/\2', line) for line in sen_after]
+
+    sen_after = [re.sub(r'★|■|#|@|\*|-|>', '', line) for line in sen_after]
+    
+    while '\n' in sen_after:
+        sen_after.remove('\n')
+    while ' \n' in sen_after:
+        sen_after.remove(' \n')
+
+    counter = len(sen_after)
+    for i in range(1,counter+1):
+        if i<10:
+            sen_after[i-1]="SID00"+ str(i)+"|"+ sen_after[i-1]
+        elif i<100:
+            sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
+        else :
+            sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
+
+
+    f = open(file_out,'w')
+    f.writelines(sen_after)
+    f.close()
+    print("Output file : ", file_out)
+    print("Processing completed !")
+
