3407396effb8712dd112628879b9da82a5e8cb2e
Chun Feng <weilin0933602899@gmail.com>
2020-10-22 11:49:02 +0800

    加入功能：使用者自訂縮寫、新增欲過濾的 semantic type

--- mytool/cui.py
+++ mytool/cui.py
@@ -49,7 +49,7 @@ def extract_sldiID(txt_file, output=True):
     index_list = range(1, lines+1)
     # 將剛剛讀出的行內容List, 傳給 mm.extract_concepts 取出概念
     concepts, error = mm.extract_concepts(
-        sents, index_list, word_sense_disambiguation=True)
+        sents, index_list, word_sense_disambiguation=True, derivational_variants=True, user_define_acronyms=True, file_format= 'sldiID')
 
     number_for_everyone = 0
     # 每個 concept 都有的數值, 紀錄現在印到了哪個 index
@@ -63,18 +63,30 @@ def extract_sldiID(txt_file, output=True):
         Index 儲存行的資訊
         semtype 儲存 semantic type 的資訊
         '''    
-        if number_for_everyone != int(concept.index) :
-            whether_print = True
-            number_for_everyone += 1 
-        if concept.semtypes == "[clnd]" or concept.semtypes == "[dsyn]" :
-            if whether_print:
+        if hasattr(concept, 'mm'):
+            if number_for_everyone != int(concept.index) :
+                whether_print = True
+                number_for_everyone += 1 
+            if (concept.semtypes == "[clnd]" or concept.semtypes == "[dsyn]" or concept.semtypes == "[acab]"
+                or concept.semtypes == "[anab]" or concept.semtypes == "[fndg]" or concept.semtypes == "[inpo]"
+                or concept.semtypes == "[mobd]" or concept.semtypes == "[neop]" or concept.semtypes == "[patf]"
+                or concept.semtypes == "[sosy]" or concept.semtypes == "[aapp]" or concept.semtypes == "[antb]"
+                or concept.semtypes == "[bacs]" or concept.semtypes == "[chem]" or concept.semtypes == "[enzy]"
+                or concept.semtypes == "[hops]" or concept.semtypes == "[horm]" or concept.semtypes == "[imft]"
+                or concept.semtypes == "[inch]" or concept.semtypes == "[lbpr]" or concept.semtypes == "[medd]"
+                or concept.semtypes == "[nnon]" or concept.semtypes == "[orch]" or concept.semtypes == "[phsu]"
+                or concept.semtypes == "[topp]" or concept.semtypes == "[vita]") :
+            # if True :
+                if whether_print:
+                    if output:
+                        print("Index:"+concept.index)
+                    whether_print =False
+                
                 if output:
-                    print("Index:"+concept.index)
-                whether_print =False
-            
-            if output:
-                print("CUI:"+concept.cui, "SMT:"+concept.semtypes, "POS:"+concept.pos_info, 
-                "PREFER_N:"+concept.preferred_name, "NEGATED:"+mmip.trigger_parser(concept.trigger)[5], "TRIGGER:"+concept.trigger)
-            cui_list.append(concept.cui)
+                    print("CUI:"+concept.cui, "SMT:"+concept.semtypes, "POS:"+concept.pos_info, 
+                    "PREFER_N:"+concept.preferred_name, "NEGATED:"+mmip.trigger_parser(concept.trigger)[5], "TRIGGER:"+concept.trigger)
+                cui_list.append(concept.cui)
+        elif hasattr(concept, 'ua'):
+            print("This is UDA !")
     return cui_list
     # Output format is : index, mm, score, preferred_name, cui, semtype, trigger, location, pos_info, tree_nodes
