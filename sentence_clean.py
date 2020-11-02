eb7a46f4197094b3b4138dbd9d739654f4298657
Chun Feng <weilin0933602899@gmail.com>
2020-11-02 14:15:21 +0800

    加入功能：輸出的文本加上 SID

--- sentence_clean.py
+++ sentence_clean.py
@@ -16,7 +16,7 @@ out_file = 'ex1_o.txt'
 sen1 = "cycle 2 on 2017.07.14 to 2017.07.1 cycle 3 on 2017.08.11 to 2017.08.13 ;"
 
 
-def read_file(txt_file):
+def file_process(txt_file):
     f = open(txt_file)
     lines = f.readlines()
     # print(type(lines))
@@ -34,11 +34,26 @@ def read_file(txt_file):
     
     sen_after = [re.sub(r'(\d{4}).(\d{2})', r'\1/\2', line) for line in sen_after]
 
-    sen_after = [re.sub (r'★|■|#', '', line) for line in sen_after]
+    sen_after = [re.sub(r'★|■|#|@|\*|-|>', '', line) for line in sen_after]
     
+    while '\n' in sen_after:
+        sen_after.remove('\n')
+    while ' \n' in sen_after:
+        sen_after.remove(' \n')
+
+    counter = len(sen_after)
+    for i in range(1,counter+1):
+        if i<10:
+            sen_after[i-1]="SID00"+ str(i)+"|"+ sen_after[i-1]
+        elif i<100:
+            sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
+        else :
+            sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
+
+
     return sen_after
 
-sen_after = read_file(file)
+sen_after = file_process(file)
 print("sen_after : ",sen_after)
 
 print('\n')
@@ -46,7 +61,7 @@ print(sen_before)
 
 def save_file():
     f = open(out_file,'w')
-    f.writelines(sen_before)
+    f.writelines(sen_after)
     f.close()
 
-# save_file()
+save_file()
