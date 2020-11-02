cda635abedfeb57415261dbe9f14aa80c9dd8d77
Chun Feng <weilin0933602899@gmail.com>
2020-11-02 17:33:38 +0800

    加入功能：可以一次處理多檔案 及 單一檔案

--- mytool/sentenizer.py
+++ mytool/sentenizer.py
@@ -17,7 +17,57 @@ sen_after = list()
 
 # sen1 = "cycle 2 on 2017.07.14 to 2017.07.1 cycle 3 on 2017.08.11 to 2017.08.13 ;"
 
-def file_process(file_in, file_out):
+def file_process(patient_id, total_days):
+    for cnt in range(1, total_days):
+        file_in = str(patient_id)+'/80001-'+str(cnt)+'.txt'
+        file_out = str(patient_id)+'/80001-'+str(cnt)+'_o.txt'
+        try:
+            f = open(file_in)
+        except FileNotFoundError:
+            print("File not found! \nPlease make sure the path is correct and file indeed exist.")
+            break
+        lines = f.readlines()
+        print("Input file : ", file_in)
+        # print(type(lines))
+        for line in lines:
+            sen_before.append(line)
+        f.close()
+        # sen_after= [re.sub(pattern, '/', line) for line in sen_before]
+        sen_after = [re.sub(r'(\d{4}) . (\d{2}) . (\d{2}) .', r'\1/\2/\3', line) for line in sen_before]
+
+        sen_after = [re.sub(r'(\d{4}).(\d{2}).(\d{2})', r'\1/\2/\3', line) for line in sen_after]
+
+        sen_after = [re.sub(r'(\d{4}).(\d{2}).(\d{1})', r'\1/\2/\3', line) for line in sen_after]
+
+        sen_after = [re.sub(r'(\d{4}) . (\d{2})', r'\1/\2', line) for line in sen_after]
+        
+        sen_after = [re.sub(r'(\d{4}).(\d{2})', r'\1/\2', line) for line in sen_after]
+
+        sen_after = [re.sub(r'★|■|#|@|\*|-|>', '', line) for line in sen_after]
+        
+        while '\n' in sen_after:
+            sen_after.remove('\n')
+        while ' \n' in sen_after:
+            sen_after.remove(' \n')
+
+        counter = len(sen_after)
+        for i in range(1,counter+1):
+            if i<10:
+                sen_after[i-1]="SID00"+ str(i)+"|"+ sen_after[i-1]
+            elif i<100:
+                sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
+            else :
+                sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
+
+
+        f = open(file_out,'w')
+        f.writelines(sen_after)
+        f.close()
+        print("Output file : ", file_out)
+        print("Processing completed !")
+
+def single_txt_process(file_in, file_out):
+    
     f = open(file_in)
     lines = f.readlines()
     print("Input file : ", file_in)
@@ -58,4 +108,3 @@ def file_process(file_in, file_out):
     f.close()
     print("Output file : ", file_out)
     print("Processing completed !")
-
