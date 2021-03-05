381bac82b8e1871b0e4cde058aa243bab0f4fbdb
Chun Feng <weilin0933602899@gmail.com>
2021-03-05 15:43:55 +0800

    更新功能
    Debug + 部份功能重寫

--- mytool/mmi_parser.py
+++ mytool/mmi_parser.py
@@ -21,8 +21,27 @@ def trigger_parser(input_phrase):
     info[4] = Part os Speech
     info[5] = Negation flag
     '''
-    for n in range(5, 11):
-        if info[n] == '1' or info[n] == '0':
-            return(info[n])
-            break
- 
+    if len(info) > 6:
+        return info[len(info)-1]
+    else :
+        return info[5]
+    # for n in range(5, 11):
+    #     if info[n] == '1' or info[n] == '0':
+    #         return(info[n])
+    #         break
+
+def trigger_parser_getinfo(input_phrase, target_info_num):
+    input_phrase = input_phrase.replace('[', '').replace(']', '').replace(',', '-')
+    info = list(input_phrase.split('-'))
+    '''
+    info[0] = UMLS concept
+    info[1] = loc
+    info[2] = locPos
+    info[3] = text
+    info[4] = Part os Speech
+    info[5] = Negation flag
+    '''
+    if len(info) > 6:        
+        return (info[len(info)-3])
+    else:
+        return(info[3])
