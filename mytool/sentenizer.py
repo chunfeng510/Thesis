2b16b5dd813a221fbba277409f842862b51f0145
Chun Feng <weilin0933602899@gmail.com>
2021-05-11 14:21:30 +0800

    功能修改

--- mytool/sentenizer.py
+++ mytool/sentenizer.py
@@ -17,12 +17,13 @@ sen_after = list()
 
 # sen1 = "cycle 2 on 2017.07.14 to 2017.07.1 cycle 3 on 2017.08.11 to 2017.08.13 ;"
 
-def file_process(patient_id, total_days):
+def file_process(patient_id, total_days, id_insert = True):
     for cnt in range(1, total_days+1):
         sen_before = list()
         sen_after = list()
         file_in = 'Clinical_Note/'+str(patient_id)+'/'+str(cnt)+'.txt'
         file_out = 'Clinical_Note/'+str(patient_id)+'/output/'+str(cnt)+'_o.txt'
+        file_out_2 = 'Clinical_Note/'+str(patient_id)+'/output_no_sid/'+str(cnt)+'_o.txt'
         try:
             f = open(file_in)
         except FileNotFoundError:
@@ -49,28 +50,36 @@ def file_process(patient_id, total_days):
         
         sen_after = [re.sub(r'\\|\[|\]|\+|\$|%|\(|\)|\^|\!|＼|／|○|↓|\||▽|:|—|！|，|。|？|、|~|￥|…|（|）|＜|＞|&|╴|│', ' ', line) for line in sen_after]
 
+        sen_after = [line.lstrip() for line in sen_after]
         while '\n' in sen_after:
             sen_after.remove('\n')
-        while ' \n' in sen_after:
-            sen_after.remove(' \n')
+        # while ' \n' in sen_after:
+        #     sen_after.remove(' \n')
 
-        counter = len(sen_after)
-        for i in range(1,counter+1):
-            if i<10:
-                sen_after[i-1]="SID00"+ str(i)+"|"+ sen_after[i-1]
-            elif i<100:
-                sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
-            else :
-                sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
+        if id_insert:
+            counter = len(sen_after)
+            for i in range(1,counter+1):
+                if i<10:
+                    sen_after[i-1]="SID00"+ str(i)+"|"+ sen_after[i-1]
+                elif i<100:
+                    sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
+                else :
+                    sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
 
+        if id_insert:
+            f = open(file_out,'w')
+            f.writelines(sen_after)
+            f.close()
+            print("Output file : ", file_out)
+            print("Processing completed !")
+        else:
+            f = open(file_out_2,'w')
+            f.writelines(sen_after)
+            f.close()
+            print("Output file without SID : ", file_out)
+            print("Output file without SID processing completed !")
 
-        f = open(file_out,'w')
-        f.writelines(sen_after)
-        f.close()
-        print("Output file : ", file_out)
-        print("Processing completed !")
-
-def single_txt_process(file_in, file_out):
+def single_txt_process(file_in, file_out,):
     
     f = open(file_in)
     lines = f.readlines()
@@ -100,6 +109,7 @@ def single_txt_process(file_in, file_out):
     while ' \n' in sen_after:
         sen_after.remove(' \n')
 
+    
     counter = len(sen_after)
     for i in range(1,counter+1):
         if i<10:
@@ -108,6 +118,7 @@ def single_txt_process(file_in, file_out):
             sen_after[i-1]="SID0"+ str(i)+"|"+ sen_after[i-1]
         else :
             sen_after[i-1]="SID"+ str(i)+"|"+ sen_after[i-1]
+    
 
 
     f = open(file_out,'w')
@@ -115,3 +126,4 @@ def single_txt_process(file_in, file_out):
     f.close()
     print("Output file : ", file_out)
     print("Processing completed !")
+
--- mytool/tocsv.py
+++ mytool/tocsv.py
@@ -1,5 +1,22 @@
 import csv
-csvfile = open('80001/output/1.csv', 'w', newline='')
-writer = csv.writer(csvfile)
-writer.writerow(['CUI', 'ROW', 'POSITION', 'NEGATION', 'SMT'])
-writer.writerow(['C0015472', 10, '14-20', 1, 'clnd'])
+# csvfile = open('80001/output/1.csv', 'w', newline='')
+# writer = csv.writer(csvfile)
+# writer.writerow(['CUI', 'ROW', 'POSITION', 'NEGATION', 'SMT'])
+# writer.writerow(['C0015472', 10, '14-20', 1, 'clnd'])
+
+def score2csv(file_in, ):
+
+    file_in = file_in
+    file_out = file_in.replace('.txt', '.csv')
+    f = open(file_in, 'r')
+    csvfile = open(file_out, 'w', newline='')
+    writer = csv.writer(csvfile)
+    writer.writerow(['SCORE', 'CUI1', 'CUI2'])
+
+    for i in f:
+        res = i.replace('\n', '').split('<>', 2)
+        writer.writerow([res[0], res[1], res[2]])
+        print(res[0], res[1], res[2])
+    csvfile.close()
+
+# score2csv('score_test.txt')
