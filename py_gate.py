b1786bf2a2e1010f7aed3e58ef267335c415c122
Chun Feng <weilin0933602899@gmail.com>
2021-05-26 11:35:50 +0800

    Delete: Unnecessary file

--- py_gate.py
+++
@@ -1,3 +0,0 @@
-from gatenlp import Document
-doc = Document.load("https://gatenlp.github.io/python-gatenlp/testdocument1.txt")
-print(doc)
--- py_nltk_try.py
+++
@@ -1,8 +0,0 @@
-from nltk import ngrams
-
-str = "This is @a wonderful@ day"
-bigram = ngrams(str.split('@'), 2)
-
-for grams in bigram:
-    print(grams)
-# print(bigram)
